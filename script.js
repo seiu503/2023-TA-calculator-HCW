3abd0f05ec711ecd0d990089afd243fc4ca748bf
sarah <rifkegribenes@gmail.com>
2023-09-13 12:16:32 -0700

    fixed data type issues in userObj find function

--- script.js
+++ script.js
@@ -1,4 +1,405 @@
-const lookup = [];
+const lookup = [
+  {
+    "userHours": 1,
+    "eocHourly": 20.77,
+    "locTotal": 2030,
+    "increaseOverCurrent": 426,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 2,
+    "eocHourly": 20.77,
+    "locTotal": 4060,
+    "increaseOverCurrent": 853,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 3,
+    "eocHourly": 20.77,
+    "locTotal": 6090,
+    "increaseOverCurrent": 1279,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 4,
+    "eocHourly": 20.77,
+    "locTotal": 8120,
+    "increaseOverCurrent": 1706,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 5,
+    "eocHourly": 20.77,
+    "locTotal": 10150,
+    "increaseOverCurrent": 2132,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 6,
+    "eocHourly": 20.77,
+    "locTotal": 12180,
+    "increaseOverCurrent": 2559,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 7,
+    "eocHourly": 20.77,
+    "locTotal": 14211,
+    "increaseOverCurrent": 2985,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 8,
+    "eocHourly": 20.77,
+    "locTotal": 16241,
+    "increaseOverCurrent": 3412,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 9,
+    "eocHourly": 20.77,
+    "locTotal": 18271,
+    "increaseOverCurrent": 3838,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 10,
+    "eocHourly": 20.77,
+    "locTotal": 20301,
+    "increaseOverCurrent": 4265,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 11,
+    "eocHourly": 20.77,
+    "locTotal": 22331,
+    "increaseOverCurrent": 4691,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 12,
+    "eocHourly": 20.77,
+    "locTotal": 24361,
+    "increaseOverCurrent": 5118,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 13,
+    "eocHourly": 20.77,
+    "locTotal": 26391,
+    "increaseOverCurrent": 5544,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 14,
+    "eocHourly": 20.77,
+    "locTotal": 28421,
+    "increaseOverCurrent": 5971,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 15,
+    "eocHourly": 20.77,
+    "locTotal": 30451,
+    "increaseOverCurrent": 6397,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 16,
+    "eocHourly": 20.77,
+    "locTotal": 32481,
+    "increaseOverCurrent": 6824,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 17,
+    "eocHourly": 20.77,
+    "locTotal": 34511,
+    "increaseOverCurrent": 7250,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 18,
+    "eocHourly": 20.77,
+    "locTotal": 36541,
+    "increaseOverCurrent": 7677,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 19,
+    "eocHourly": 20.77,
+    "locTotal": 38572,
+    "increaseOverCurrent": 8103,
+    "numCOLAs": 2,
+    "numSteps": 0
+  },
+  {
+    "userHours": 20,
+    "eocHourly": 21.77,
+    "locTotal": 41642,
+    "increaseOverCurrent": 8530,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 21,
+    "eocHourly": 21.77,
+    "locTotal": 43724,
+    "increaseOverCurrent": 8956,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 22,
+    "eocHourly": 21.77,
+    "locTotal": 45806,
+    "increaseOverCurrent": 9383,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 23,
+    "eocHourly": 21.77,
+    "locTotal": 47888,
+    "increaseOverCurrent": 9809,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 24,
+    "eocHourly": 21.77,
+    "locTotal": 49970,
+    "increaseOverCurrent": 10236,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 25,
+    "eocHourly": 21.77,
+    "locTotal": 52052,
+    "increaseOverCurrent": 10662,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 26,
+    "eocHourly": 21.77,
+    "locTotal": 54134,
+    "increaseOverCurrent": 11088,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 27,
+    "eocHourly": 21.77,
+    "locTotal": 56216,
+    "increaseOverCurrent": 11515,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 28,
+    "eocHourly": 21.77,
+    "locTotal": 58298,
+    "increaseOverCurrent": 11941,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 29,
+    "eocHourly": 21.77,
+    "locTotal": 60380,
+    "increaseOverCurrent": 12368,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 30,
+    "eocHourly": 21.77,
+    "locTotal": 62462,
+    "increaseOverCurrent": 12794,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 31,
+    "eocHourly": 21.77,
+    "locTotal": 64544,
+    "increaseOverCurrent": 13221,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 32,
+    "eocHourly": 21.77,
+    "locTotal": 66627,
+    "increaseOverCurrent": 13647,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 33,
+    "eocHourly": 21.77,
+    "locTotal": 68709,
+    "increaseOverCurrent": 14074,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 34,
+    "eocHourly": 21.77,
+    "locTotal": 70791,
+    "increaseOverCurrent": 14500,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 35,
+    "eocHourly": 21.77,
+    "locTotal": 72873,
+    "increaseOverCurrent": 14927,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 36,
+    "eocHourly": 21.77,
+    "locTotal": 74955,
+    "increaseOverCurrent": 15353,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 37,
+    "eocHourly": 21.77,
+    "locTotal": 77037,
+    "increaseOverCurrent": 15780,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 38,
+    "eocHourly": 21.77,
+    "locTotal": 79119,
+    "increaseOverCurrent": 16206,
+    "numCOLAs": 2,
+    "numSteps": 1
+  },
+  {
+    "userHours": 39,
+    "eocHourly": 22.77,
+    "locTotal": 83229,
+    "increaseOverCurrent": 16633,
+    "numCOLAs": 2,
+    "numSteps": 2
+  },
+  {
+    "userHours": 40,
+    "eocHourly": 22.77,
+    "locTotal": 85363,
+    "increaseOverCurrent": 17059,
+    "numCOLAs": 2,
+    "numSteps": 2
+  },
+  {
+    "userHours": 41,
+    "eocHourly": 22.77,
+    "locTotal": 87497,
+    "increaseOverCurrent": 17486,
+    "numCOLAs": 2,
+    "numSteps": 2
+  },
+  {
+    "userHours": 42,
+    "eocHourly": 22.77,
+    "locTotal": 89631,
+    "increaseOverCurrent": 17912,
+    "numCOLAs": 2,
+    "numSteps": 2
+  },
+  {
+    "userHours": 43,
+    "eocHourly": 22.77,
+    "locTotal": 91765,
+    "increaseOverCurrent": 18339,
+    "numCOLAs": 2,
+    "numSteps": 2
+  },
+  {
+    "userHours": 44,
+    "eocHourly": 22.77,
+    "locTotal": 93900,
+    "increaseOverCurrent": 18765,
+    "numCOLAs": 2,
+    "numSteps": 2
+  },
+  {
+    "userHours": 45,
+    "eocHourly": 22.77,
+    "locTotal": 96034,
+    "increaseOverCurrent": 19192,
+    "numCOLAs": 2,
+    "numSteps": 2
+  },
+  {
+    "userHours": 46,
+    "eocHourly": 22.77,
+    "locTotal": 98168,
+    "increaseOverCurrent": 19618,
+    "numCOLAs": 2,
+    "numSteps": 2
+  },
+  {
+    "userHours": 47,
+    "eocHourly": 22.77,
+    "locTotal": 100302,
+    "increaseOverCurrent": 20045,
+    "numCOLAs": 2,
+    "numSteps": 2
+  },
+  {
+    "userHours": 48,
+    "eocHourly": 22.77,
+    "locTotal": 102436,
+    "increaseOverCurrent": 20471,
+    "numCOLAs": 2,
+    "numSteps": 2
+  },
+  {
+    "userHours": 49,
+    "eocHourly": 22.77,
+    "locTotal": 104570,
+    "increaseOverCurrent": 20898,
+    "numCOLAs": 2,
+    "numSteps": 2
+  },
+  {
+    "userHours": 50,
+    "eocHourly": 22.77,
+    "locTotal": 106704,
+    "increaseOverCurrent": 21324,
+    "numCOLAs": 2,
+    "numSteps": 2
+  }
+];
 
 // set global variables
 let userHours = 0;
@@ -8,7 +409,9 @@ let endHourlyRate = 0;
 let payIncreaseDollar = 0;
 let payIncreasePercent = 0;
 let lifeOfContractTotal = 0;
+let userLang = "en-US";
 let userLangCode = "en";
+let userObj = {};
 
 // translation object
 const trans = {
@@ -68,9 +471,17 @@ const trans = {
     vi: "Mức tăng thu nhập của bạn trong suốt thời hạn hợp đồng được ước tính là ",
     zh: "在合同期内，您的收入预计将增加 "
   },
+  val1: {
+    en: "Please select the average number of hours you work per week",
+    es: "Por favor seleccione el número promedio de horas que trabaja por semana",
+    ru: "Пожалуйста, выберите среднее количество часов, которые вы работаете в неделю", 
+    vi: "Vui lòng chọn số giờ trung bình bạn làm việc mỗi tuần",
+    zh: "请选择您每周的平均工作时间"
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function(){
+  console.log('dom loaded');
 
   // save elements to variables for later access
   let displayEl = document.getElementById("display");
@@ -86,27 +497,35 @@ document.addEventListener("DOMContentLoaded", function(){
   let languagePicker = document.getElementsByClassName('gt_selector')[0];
 
   // set language code
-  if (languagePicker) {
-    console.log(`languagePicker: ${languagePicker.value}`);
-    userLang = languagePicker.value;
-  } else {
-    userLang = navigator.language || navigator.userLanguage; 
-    console.log(`userLang: ${userLang}`);
+  const setLanguageCode = () => {
+    if (languagePicker) {
+      console.log(`languagePicker: ${languagePicker.value}`);
+      userLang = languagePicker.value;
+      userLangCode = userLang.substring(3).toLowerCase();
+      console.log(`userLang LP: ${userLang}`);
+      console.log(`userLangCode LP: ${userLangCode}`);
+    } else {
+      userLang = navigator.language || navigator.userLanguage; 
+      userLangCode = userLang.substring(0,2).toLowerCase();
+      console.log(`userLang NOLP: ${userLang}`);
+      console.log(`userLangCode NOLP: ${userLangCode}`);
+    }
+
+    const langCodeOptions = ['en', 'es', 'ru', 'vi', 'zh'];
+
+    // default to english if user language is not supported
+    if (!langCodeOptions.includes(userLangCode)) {
+      console.log('userLangCode not in array');
+      userLangCode = "en";
+      console.log(`userLangCode: ${userLangCode}`);
+    };
   }
-
-  let userLangCode = userLang.substring(0,2).toLowerCase();
-  console.log(`userLangCode: ${userLangCode}`);
-
-  const langCodeOptions = ['en', 'es', 'ru', 'vi', 'zh'];
-
-  // default to english if user language is not supported
-  if (!langCodeOptions.includes('userLangCode')) {
-    userLangCode = "en";
-    console.log(`userLangCode: ${userLangCode}`);
-  };
+  setLanguageCode();
+  
 
   // generate list of hours options
-  let hoursOptions = Array.from(Array(50).keys());
+  let hoursOptions = Array.from(Array(61).keys());
+  hoursOptions.shift();
   console.log(hoursOptions);
 
   // load list of hoursOptions
@@ -115,10 +534,22 @@ document.addEventListener("DOMContentLoaded", function(){
     hpwEl.options[hpwEl.options.length] = new Option(item, item)
   });  
 
+  // find userObj
+  const findUserObj = () => {
+    console.log('findUserObj');
+    console.log(`userHours: ${userHours}`);
+    userObj = lookup.find((item) => item['userHours'].toString() === userHours );
+    if (!userObj) { userObj = {} };
+    console.log('userObj');
+    console.log(userObj);
+  };
+
   // listen for changes to hours worked
   function hoursChange(e) {
+    console.log('hoursChange');
     userHours = e.target.value;
     console.log(`userHours: ${userHours}`);
+    findUserObj();
   }
 
   // clean currency strings
@@ -130,30 +561,34 @@ document.addEventListener("DOMContentLoaded", function(){
     }
   };
 
+
+
+
   // generate results string and message
   function resultsString() {
     // set variables
-    userObj = lookupUserObject(userPosition, userStep);
-    currentMonthlyBase = clean(userObj['Current monthly pay']);
-    console.log(`currentMonthlyBase: ${currentMonthlyBase}`);
-    projectedMonthlyBase2024 = clean(userObj['2024 monthly pay']);
-    console.log(`projectedMonthlyBase2024: ${projectedMonthlyBase2024}`);
-    const projectedMonthlyBase2025 = clean(userObj['2025 monthly pay']);
-    console.log(`projectedMonthlyBase2025: ${projectedMonthlyBase2025}`);
-    projectedMonthlyIncrease2024 = clean(userObj['2024 monthly difference (2024-current)']);
-    console.log(`projectedMonthlyIncrease2024: ${projectedMonthlyIncrease2024}`);
-    annualIncrease2025 = clean(userObj["Annual pay at monthly pay at end of contract"]) - clean(userObj["Annual pay at current monthly pay"]);
-    console.log(`annualIncrease2025: ${annualIncrease2025}`);
-    const monthlyIncrease2025 = clean(userObj['2025 monthly difference (2025 - current)']);
-    console.log(`monthlyIncrease2025: ${monthlyIncrease2025}`);
-    const percentRaise = (((projectedMonthlyBase2025 - currentMonthlyBase)/currentMonthlyBase) * 100).toFixed(2);
-    console.log(percentRaise);
+    let numCOLAs = userObj['numCOLAs'] || 0;
+    let numSteps = userObj['numSteps'] || 0;
+    let eocHourly = userObj['eocHourly'] || 0;
+    let payIncreaseDollar = eocHourly - 17.77 || 0;
+    let payIncreasePercent = (payIncreaseDollar / 17.77 * 100).toFixed(0) || 0;
+    let locTotal = userObj['locTotal'] || 0;
 
-    return `<p style="max-width: 600px; margin: auto auto 20px auto;"><ul style="max-width: 600px; margin: auto;"><li style="margin-bottom:15px;">If you are ${userPositionWithArticle} on step ${userStep}, your current monthly base salary is <span class="purplebold">$${currentMonthlyBase.toLocaleString(undefined, { maximumFractionDigits: 0 })}</span>. 
-    </li><li style="margin-bottom:15px;">Upon ratification, you will receive a one-time cost-of-living payment of <span class="purplebold">$1,500</span>, before taxes.
-    </li><li style="margin-bottom:15px;">After the first COLA in December 2023 and any steps you qualify for, your monthly salary will increase to <span class="purplebold">$${projectedMonthlyBase2024.toLocaleString(undefined, { maximumFractionDigits: 0 })}</span> by July 2024. This is a pay increase of <span class="purplebold"> $${projectedMonthlyIncrease2024.toLocaleString(undefined, { maximumFractionDigits: 0 })}</span> per month. 
-    </li><li style="margin-bottom:15px;">After the second COLA in January (or February) of 2025 and any steps you qualify for, your monthly salary will increase to <span class="purplebold">$${projectedMonthlyBase2025.toLocaleString(undefined, { maximumFractionDigits: 0 })}</span> by July 2025. This is a total pay increase of <span class="purplebold"> $${monthlyIncrease2025.toLocaleString(undefined, { maximumFractionDigits: 0 })}</span> per month, or <span class="purplebold">$${annualIncrease2025.toLocaleString(undefined, { maximumFractionDigits: 0 })}</span> per year <span class="purplebold">(${percentRaise}%)</span>.
-    </li></ul>
+    console.log(`numCOLAs: ${numCOLAs}`);
+    console.log(`numSteps: ${numSteps}`);
+    console.log(`eocHourly: ${eocHourly}`);
+    console.log(`payIncreaseDollar: ${payIncreaseDollar}`);
+    console.log(`payIncreasePercent: ${payIncreasePercent}`);
+    console.log(`locTotal: ${locTotal}`);
+
+    if (!userObj['numCOLAs']) {
+      return `<p>No data available yet for that number of hours per week</p>`
+    }
+
+    return `<p style="max-width: 600px; margin: auto auto 20px auto;"><ul style="max-width: 600px; margin: auto;"><li style="margin-bottom:15px;">${trans['p1_1'][userLangCode]}<span class="purplebold">${numCOLAs}</span>${trans['p1_2'][userLangCode]}<span class="purplebold">${numCOLAs}</span>${trans['p1_3'][userLangCode]}</li>
+    <li style="margin-bottom:15px;">${trans['p2_1'][userLangCode]}<span class="purplebold">$${eocHourly}</span>.</li>
+    <li style="margin-bottom:15px;">${trans['p3_1'][userLangCode]}<span class="purplebold">$${payIncreaseDollar.toLocaleString(undefined, { maximumFractionDigits: 0 })}</span>${trans['p3_2'][userLangCode]}<span class="purplebold">${payIncreasePercent}</span>${trans['p3_3'][userLangCode]}</li>
+    <li style="margin-bottom:15px;">${trans['p4_1'][userLangCode]}<span class="purplebold">$${locTotal.toLocaleString(undefined, { maximumFractionDigits: 0 })}</span>.</li></ul>
     </p>`
   }
 
@@ -165,13 +600,14 @@ document.addEventListener("DOMContentLoaded", function(){
   // On submit, hide instructions and display results
   function handleSubmit(e) {
     e.preventDefault();
-    if (!userPosition || !userClassificationCode || userStep == 0) {
+    if (!userHours || userHours == 0) {
+      console.log('!userHours');
       submit.setAttribute("style", "display:none;");
       startOver.setAttribute("style", "display:block;");
       instructions.setAttribute("style", "height: 0; display:none;");
       inputs.setAttribute("style", "height: 0; display:none;");
       message.setAttribute("style", "display:block;");
-      results.innerHTML = "Please select both your classification and your current step.";
+      results.innerHTML = `<h3 class=center>${trans['val1'][userLangCode]}</h3>`;
       return;
     }
     submit.setAttribute("style", "display:none;");
@@ -184,6 +620,10 @@ document.addEventListener("DOMContentLoaded", function(){
 
   submit.addEventListener("click", handleSubmit);
   startOver.addEventListener("click", handleReload);
+  if(languagePicker) {
+    languagePicker.addEventListener("change", setLanguageCode);
+  }
+  
 
   // custom select styling
 
@@ -234,12 +674,8 @@ document.addEventListener("DOMContentLoaded", function(){
               break;
             }
           }
-          if (s.getAttribute('id') === 'classification') {
-            classificationChange({ target: s });
-          };
-          
-          if (s.getAttribute('id') ==='step') {
-            stepChange({ target: s });
+          if (s.getAttribute('id') === 'hPW') {
+            hoursChange({ target: s });
           };
           h.click();
       });
